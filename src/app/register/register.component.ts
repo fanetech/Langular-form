62d5c7140aec560ac5b7784bd7cbb69ec3ad81a2
fanetech <louguefranck.20@gmail.com>
2022-11-08 22:42:50 +0000

    inital commit and ngform

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from './user';
+import {NgForm} from '@angular/forms'
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  public user: User = new User()
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public saveData(registerForm: NgForm){
+    console.log(registerForm)
+    console.log(registerForm.value)
+  }
+}
